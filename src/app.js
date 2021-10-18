85a26fe2e1931921b345100a6123e31d6cff1887
melisaclskn <melisaclskn5461@gmail.com>
2021-10-19 00:03:04 +0300

    18 Ekim Ders

---
+++ src/app.js
@@ -0,0 +1,31 @@
+// console.log("merhaba kodlama.io")
+
+// let dolarDun=9.20
+// let dolar=9.30
+
+// var dolar =9.20
+// {
+//   //  var dolar=9.10
+// }
+// console.log(dolar)//hata
+
+// let dolara=9.20
+// {
+//     let dolara=9.10
+// }
+// console.log(dolara)//9.20
+
+// const euro=11.2
+// //euro=11//hata
+// console.log(euro)
+
+//array
+//camelCasing
+//PascalCasing
+let konutKredileri=["konut kredisi","emlak konut","kamu konut "]
+
+
+for (let i=0;i<konutKredileri.length;i++) {
+    console.log(konutKredileri[i])
+    
+}
